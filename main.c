ace663cc294f4aaef9a52a68a13514b86f07504f
duarte <katmandu612@gmail.com>
2025-05-10 08:19:50 -0400

    Regorganize

--- main.c
+++ main.c
@@ -1,41 +1,5 @@
-#include <math.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <spa/param/audio/format-utils.h>
-#include <pipewire/pipewire.h>
-#include <stdatomic.h>
+#include "sound.h"
 
-
-#define DEFAULT_RATE 44100
-#define DEFAULT_CHANNELS 2
-#define DEFAULT_VOLUME 0.7
-
-enum
-{
-
-        TONE_C,
-        TONE_D,
-        TONE_E,
-        TONE_F,
-        TONE_G,
-        TONE_A,
-        TONE_B
-};
-
-static const double tones[] = {261.6256, 293.6648, 329.6276, 349.2282, 391.9954, 440.00, 493.8833};
-
-static atomic_bool running = ATOMIC_VAR_INIT(true);
-static struct pw_stream *stream = NULL;
-static struct pw_main_loop *main_loop = NULL;
-
-double frequency = 440.00;
-
-struct data
-{
-        struct pw_main_loop *loop;
-        struct pw_stream *stream;
-        double accumulator;
-};
 
 static void on_process(void *userdata)
 {
---
+++ sound.h
@@ -0,0 +1,44 @@
+#ifndef SOUND_H
+#define SOUND_H
+
+#include <math.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <spa/param/audio/format-utils.h>
+#include <pipewire/pipewire.h>
+#include <stdatomic.h>
+
+#define M_PI_M2 (M_PI + M_PI)
+
+#define DEFAULT_RATE 44100
+#define DEFAULT_CHANNELS 2
+#define DEFAULT_VOLUME 0.7
+
+enum
+{
+
+        TONE_C,
+        TONE_D,
+        TONE_E,
+        TONE_F,
+        TONE_G,
+        TONE_A,
+        TONE_B
+};
+
+static const double tones[] = {261.6256, 293.6648, 329.6276, 349.2282, 391.9954, 440.00, 493.8833};
+
+static atomic_bool running = ATOMIC_VAR_INIT(true);
+static struct pw_stream *stream = NULL;
+static struct pw_main_loop *main_loop = NULL;
+
+double frequency = 440.00;
+
+struct data
+{
+        struct pw_main_loop *loop;
+        struct pw_stream *stream;
+        double accumulator;
+};
+
+#endif
